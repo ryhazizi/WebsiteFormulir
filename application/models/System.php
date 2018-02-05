2dea836f1749edfcc6f91e909d40052014501305
Syakirin Amin <akrinmin@gmail.com>
2018-02-05 18:02:34 +0700

    improvement

--- application/models/System.php
+++ application/models/System.php
@@ -10,60 +10,7 @@ class System extends CI_Model {
 		$this->load->database();
 	}
     
-    public function DateOpt($Type) {
-		 switch ($Type) {
-		 	case "tanggal":
-		 		$a=array();
 
-				for ($b=1;$b<=31;$b++) {
-					$a[$b]=$b;
-				}
-
-	  			return $a;
-		 	break;
-		 	case "Tanggal":
-		 		$a=array();
-
-				for ($b=1;$b<=31;$b++) {
-					$a[$b]=$b;
-				}
-
-	  			return $a;
-		 	break;
-		 	case "bulan":
-		 		return array("1" => "Januari","2" => "Februari","3" => "Maret","4" => "April","5" => "Mei","6" => "Juni","7" => "Juli","8" => "Agustus","9" => "September","10" => "Oktober","11" => "November","12" => "Desember");
-		 	break;
-		 	case "Bulan":
-		 		return array("1" => "Januari","2" => "Februari","3" => "Maret","4" => "April","5" => "Mei","6" => "Juni","7" => "Juli","8" => "Agustus","9" => "September","10" => "Oktober","11" => "November","12" => "Desember");
-		 	break;
-		 	case "tahun":
-		 		$c=array();
-
-		 		for ($d = 1950;$d <= date("Y");$d++) {
-		 		   $c[$d]=$d;
-		 		}
-
-		 		return $c;
-		 	break;
-		 	case "Tahun":
-		 		$c=array();
-
-		 		for ($d = 1950;$d <= date("Y");$d++) {
-		 		   $c[$d]=$d;
-		 		}
-
-		 		return $c;
-		 	break;
-		 	default:
-		 	   return "Tipe tidak tersedia";
-		 	break;
-		 }
-	}
-
-	public function id_orang()
-	{
-        return substr(str_shuffle("AaBbCcDdEeFfGgHhIiJjKkLlMnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"),0,30);
-	}
 
 	public function simpan_data($DataArray) {
 		return $this->db->insert("dataorang", $DataArray);
@@ -74,11 +21,12 @@ class System extends CI_Model {
 	}
 
 	public function cek_nama($Nama = NULL) {
-		if (is_null($Nama)) {
-			return "fungsi cek_nama harus memiliki parameter!";
-        }else if (!$Nama) {
-        	return "nama masih kosong!";
-        }else {
+      // sudah di validasi di controller
+// 		if (is_null($Nama)) {
+// 			return "fungsi cek_nama harus memiliki parameter!";
+//         }else if (!$Nama) {
+//         	return "nama masih kosong!";
+//         }else {
         	$this->db->select('NamaLengkap');
         	
         	$this->db->from('dataorang');
@@ -92,23 +40,23 @@ class System extends CI_Model {
         	}else {
         		return FALSE;
         	}
-        }
+    //	    }
 	}
 
 	public function hapus_data($IDyangmaudihapus) {
-		if (is_null($IDyangmaudihapus)) {
-			return "fungsi hapus_data harus memiliki parameter!";
-        }else if (!$IDyangmaudihapus) {
-        	return "id data yg mau dihapus masih kosong!";
-        }else {
-        	$this->db->select('ID');
+// 		if (is_null($IDyangmaudihapus)) {
+// 			return "fungsi hapus_data harus memiliki parameter!";
+//         }else if (!$IDyangmaudihapus) {
+//         	return "id data yg mau dihapus masih kosong!";
+//         }else {
+//         	$this->db->select('ID');
         	
-        	$this->db->from('dataorang');
+//         	$this->db->from('dataorang');
         	
         	$this->db->where('ID', $IDyangmaudihapus);
 
         	return $this->db->delete('dataorang');
-        }
+    //    }
 	}
 
 }
